aa642a9907bd2b73f9715f59306804c882666d4d
NikkitaMalyarov <nikkitam4@gmail.com>
2024-06-01 15:08:56 -0700

    trying to make the email work

---
+++ assets/js/email-sender.js
@@ -0,0 +1,9 @@
+// const recipient = '';
+// const name = document.getElementById('name');
+// const email  = document.getElementById('email');
+// const message = document.getElementById('message');
+
+// Button.addEventListener('click', function() {
+//     const message = encodeURIComponent(documnet.getElementById('message').value);
+//     this.href = `mailto:${recipient}?subject=Message from Website&body=${message}`;
+// })
